cf3e13c4b091242c0ca6eede4581934d28b49d7a
Mukundan314 <mukundan314@gmail.com>
2021-08-02 20:55:40 +0530

    feat: find and watch tfstate files

--- src/extension.ts
+++ src/extension.ts
@@ -2,12 +2,22 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
+const TFSTATE_GLOB = "**/terraform.tfstate";
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export async function activate(context: vscode.ExtensionContext) {
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "terrastate" is now active!');
+
+	const tfstateFiles = await vscode.workspace.findFiles(TFSTATE_GLOB);
+
+	const fsWatcher = vscode.workspace.createFileSystemWatcher(TFSTATE_GLOB, false, false, false);
+
+	// fsWatcher.onDidCreate(() => {})
+	// fsWatcher.onDidChange(() => {})
+	// fsWatcher.onDidDelete(() => {})
 }
 
 // this method is called when your extension is deactivated
