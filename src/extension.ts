91738d4e98a7e8796b3d5a99e17732e56738b1a7
Mukundan314 <mukundan314@gmail.com>
2021-08-04 12:40:13 +0530

    chore: setup linter and formatter

--- src/extension.ts
+++ src/extension.ts
@@ -1,20 +1,19 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
-import { TerrastateProvider } from './terrastateProvider';
+import * as vscode from "vscode";
+import { TerrastateProvider } from "./terrastateProvider";
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-export async function activate(context: vscode.ExtensionContext) {
-	const terrastateProvider = new TerrastateProvider();
-	vscode.window.registerTreeDataProvider('terrastate.terrastate', terrastateProvider);
-	vscode.commands.registerCommand('terrastate.refresh', () =>
-	terrastateProvider.refresh()
+export async function activate(
+  context: vscode.ExtensionContext
+): Promise<void> {
+  const terrastateProvider = new TerrastateProvider();
+  vscode.window.registerTreeDataProvider(
+    "terrastate.terrastate",
+    terrastateProvider
   );
-	vscode.commands.registerCommand('terrastate.apply', () =>
-	terrastateProvider.refresh()
+
+  vscode.commands.registerCommand("terrastate.refresh", () =>
+    terrastateProvider.refresh()
+  );
+  vscode.commands.registerCommand("terrastate.apply", () =>
+    terrastateProvider.refresh()
   );
 }
-
-// this method is called when your extension is deactivated
-export function deactivate() {}
--- src/terrastateProvider.ts
+++ src/terrastateProvider.ts
@@ -1,98 +1,144 @@
-import * as vscode from 'vscode';
-import { promises as fs } from 'fs';
-import * as path from 'path';
+import * as vscode from "vscode";
+import { promises as fs } from "fs";
+import * as path from "path";
 
 const TFSTATE_GLOB = "**/terraform.tfstate";
 
+type Resource = {
+  name: string;
+  type: string;
+};
+
 class Item extends vscode.TreeItem {
+  tfstateFile?: vscode.Uri;
+  resource?: Resource;
+
+  constructor({
+    type,
+    tfstateFile,
+    resource,
+  }: {
+    type: string;
     tfstateFile?: vscode.Uri;
-    resource?: any;
+    resource?: Resource;
+  }) {
+    super(
+      "",
+      type === "directory"
+        ? vscode.TreeItemCollapsibleState.Expanded
+        : vscode.TreeItemCollapsibleState.None
+    );
 
-    constructor({ type, tfstateFile, resource }: { type: string, tfstateFile?: vscode.Uri, resource?: any  }) {
-        super('', type === "directory" ? vscode.TreeItemCollapsibleState.Expanded : vscode.TreeItemCollapsibleState.None);
-
-        switch (type) {
-            case "directory":
-                this.label = tfstateFile ? path.dirname(vscode.workspace.asRelativePath(tfstateFile.path, true)) : '';
-                this.iconPath = path.join(__filename, '../../media/terraform.svg'); 
-                break;
-            case "resource":
-                this.label = resource.name;
-                this.description = resource.type;
-                this.iconPath = new vscode.ThemeIcon("debug-start");
-                break;
-            case "none":
-                this.description = '(No resources deployed)';
-                break;
-            case "error":
-                this.description = '(Failed to load tfstate)';
-                break;
-            default:
-                throw new Error(`Invalid type ${type}`);
-        }
-
-        this.contextValue = type;
-        this.tfstateFile = tfstateFile;
-        this.resource = resource;
+    switch (type) {
+      case "directory":
+        this.label = tfstateFile
+          ? path.dirname(
+              vscode.workspace.asRelativePath(tfstateFile.path, true)
+            )
+          : "";
+        this.iconPath = path.join(__filename, "../../media/terraform.svg");
+        break;
+      case "resource":
+        this.label = resource.name;
+        this.description = resource.type;
+        this.iconPath = new vscode.ThemeIcon("debug-start");
+        break;
+      case "none":
+        this.description = "(No resources deployed)";
+        break;
+      case "error":
+        this.description = "(Failed to load tfstate)";
+        break;
+      default:
+        throw new Error(`Invalid type ${type}`);
     }
 
+    this.contextValue = type;
+    this.tfstateFile = tfstateFile;
+    this.resource = resource;
+  }
 }
 
 export class TerrastateProvider implements vscode.TreeDataProvider<Item> {
-    private _onDidChangeTreeData: vscode.EventEmitter<Item | undefined | null | void> = new vscode.EventEmitter<Item | undefined | null | void>();
-    readonly onDidChangeTreeData: vscode.Event<Item | undefined | null | void> = this._onDidChangeTreeData.event;
+  private _onDidChangeTreeData: vscode.EventEmitter<
+    Item | undefined | null | void
+  > = new vscode.EventEmitter<Item | undefined | null | void>();
+  readonly onDidChangeTreeData: vscode.Event<Item | undefined | null | void> =
+    this._onDidChangeTreeData.event;
 
-    private rootItems: Item[] = [];
+  private rootItems: Item[] = [];
 
-    constructor() {
-        const fsWatcher = vscode.workspace.createFileSystemWatcher(TFSTATE_GLOB, false, false, false);
+  constructor() {
+    const fsWatcher = vscode.workspace.createFileSystemWatcher(
+      TFSTATE_GLOB,
+      false,
+      false,
+      false
+    );
 
-        fsWatcher.onDidCreate(created => {
-            this.rootItems.push(new Item({ type: 'directory', tfstateFile: created }));
-            this.rootItems.sort((a, b) => ((a.label || "") > (b.label || "")) ? 1 : -1);
-            this._onDidChangeTreeData.fire();
-        });
+    fsWatcher.onDidCreate((created) => {
+      this.rootItems.push(
+        new Item({ type: "directory", tfstateFile: created })
+      );
+      this.rootItems.sort((a, b) =>
+        (a.label || "") > (b.label || "") ? 1 : -1
+      );
+      this._onDidChangeTreeData.fire();
+    });
 
-        fsWatcher.onDidDelete(deleted => {
-            this.rootItems = this.rootItems.filter(({ tfstateFile }) => tfstateFile?.path !== deleted.path);
-            this._onDidChangeTreeData.fire();
-        });
+    fsWatcher.onDidDelete((deleted) => {
+      this.rootItems = this.rootItems.filter(
+        ({ tfstateFile }) => tfstateFile?.path !== deleted.path
+      );
+      this._onDidChangeTreeData.fire();
+    });
 
-        fsWatcher.onDidChange(changed => {
-            this._onDidChangeTreeData.fire(this.rootItems.filter(({ tfstateFile }) => tfstateFile?.path === changed.path)[0]);
-        });
+    fsWatcher.onDidChange((changed) => {
+      this._onDidChangeTreeData.fire(
+        this.rootItems.filter(
+          ({ tfstateFile }) => tfstateFile?.path === changed.path
+        )[0]
+      );
+    });
 
-        setInterval(() => {
-            vscode.workspace.findFiles(TFSTATE_GLOB).then(files => {
-                this.rootItems = files.map(uri => new Item({type: 'directory', tfstateFile: uri}));
-                this.rootItems.sort((a, b) => ((a.label || "") > (b.label || "")) ? 1 : -1);
-                this._onDidChangeTreeData.fire();
-            });
-        }, 1000);
+    setInterval(() => {
+      vscode.workspace.findFiles(TFSTATE_GLOB).then((files) => {
+        this.rootItems = files.map(
+          (uri) => new Item({ type: "directory", tfstateFile: uri })
+        );
+        this.rootItems.sort((a, b) =>
+          (a.label || "") > (b.label || "") ? 1 : -1
+        );
+        this._onDidChangeTreeData.fire();
+      });
+    }, 1000);
+  }
+
+  async getChildren(element?: Item): Promise<Item[]> {
+    if (element?.tfstateFile) {
+      try {
+        const data = JSON.parse(
+          (await fs.readFile(element.tfstateFile.fsPath)).toString()
+        );
+        const items = data.resources.map(
+          (resource: Resource) =>
+            new Item({ type: "resource", resource: resource })
+        );
+
+        return items.length ? items : [new Item({ type: "none" })];
+      } catch (err) {
+        return [new Item({ type: "error" })];
+      }
     }
 
-    async getChildren(element?: Item): Promise<Item[]> {
-        if (element?.tfstateFile) {
-            try {
-                const data = JSON.parse((await fs.readFile(element.tfstateFile.fsPath)).toString());
-                const items = data.resources.map(
-                    (resource: any) => new Item({ type: 'resource', resource: resource })
-                );
+    return this.rootItems;
+  }
 
-                return items.length ? items : [new Item({ type: "none" })];
-            } catch (err) {
-                return [new Item({ type: "error" })];
-            }
-        }
+  getTreeItem(element: Item): vscode.TreeItem {
+    return element;
+  }
 
-        return this.rootItems;
-    }
-
-    getTreeItem(element: Item): vscode.TreeItem {
-        return element;
-    }
-
-    refresh(): null {
-        return null;
-    }
+  refresh(): null {
+    return null;
+  }
 }
