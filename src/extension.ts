38bc7a14db99e0e8053d4207c8833feba9032e43
Mukundan314 <mukundan314@gmail.com>
2021-08-14 09:13:18 +0530

    improv: push disposables into context.subscriptions

--- src/extension.ts
+++ src/extension.ts
@@ -4,7 +4,6 @@ import { TerrastateProvider } from "./terrastateProvider";
 import { GraphProvider } from "./graphProvider";
 
 export async function activate(
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   context: vscode.ExtensionContext
 ): Promise<void> {
   if (!(await setTerraformPath())) {
@@ -12,93 +11,117 @@ export async function activate(
   }
 
   const terrastateProvider = new TerrastateProvider();
-
-  vscode.window.registerTreeDataProvider(
-    "terrastate.terrastate",
-    terrastateProvider
+  context.subscriptions.push(
+    vscode.window.registerTreeDataProvider(
+      "terrastate.terrastate",
+      terrastateProvider
+    )
   );
 
   const graphProvider = new GraphProvider();
-
-  vscode.window.registerTreeDataProvider("terrastate.graph", graphProvider);
-
-  vscode.commands.registerCommand(
-    "terrastate.refresh",
-    terrastateProvider.refresh.bind(terrastateProvider)
+  context.subscriptions.push(
+    vscode.window.registerTreeDataProvider("terrastate.graph", graphProvider)
   );
 
-  vscode.commands.registerCommand(
-    "terrastate.init",
-    terrastateProvider.init.bind(terrastateProvider)
+  context.subscriptions.push(
+    vscode.commands.registerCommand(
+      "terrastate.refresh",
+      terrastateProvider.refresh.bind(terrastateProvider)
+    )
   );
 
-  vscode.commands.registerCommand(
-    "terrastate.validate",
-    terrastateProvider.validate.bind(terrastateProvider)
+  context.subscriptions.push(
+    vscode.commands.registerCommand(
+      "terrastate.init",
+      terrastateProvider.init.bind(terrastateProvider)
+    )
   );
 
-  vscode.commands.registerCommand(
-    "terrastate.apply",
-    terrastateProvider.apply.bind(terrastateProvider)
+  context.subscriptions.push(
+    vscode.commands.registerCommand(
+      "terrastate.validate",
+      terrastateProvider.validate.bind(terrastateProvider)
+    )
   );
 
-  vscode.commands.registerCommand(
-    "terrastate.applyAll",
-    terrastateProvider.apply.bind(terrastateProvider)
+  context.subscriptions.push(
+    vscode.commands.registerCommand(
+      "terrastate.apply",
+      terrastateProvider.apply.bind(terrastateProvider)
+    )
   );
 
-  vscode.commands.registerCommand(
-    "terrastate.destroy",
-    terrastateProvider.destroy.bind(terrastateProvider)
+  context.subscriptions.push(
+    vscode.commands.registerCommand(
+      "terrastate.applyAll",
+      terrastateProvider.apply.bind(terrastateProvider)
+    )
   );
 
-  vscode.commands.registerCommand(
-    "terrastate.destroyAll",
-    terrastateProvider.destroy.bind(terrastateProvider)
+  context.subscriptions.push(
+    vscode.commands.registerCommand(
+      "terrastate.destroy",
+      terrastateProvider.destroy.bind(terrastateProvider)
+    )
   );
 
-  vscode.commands.registerCommand(
-    "terrastate.taint",
-    terrastateProvider.taint.bind(terrastateProvider)
+  context.subscriptions.push(
+    vscode.commands.registerCommand(
+      "terrastate.destroyAll",
+      terrastateProvider.destroy.bind(terrastateProvider)
+    )
   );
 
-  vscode.commands.registerCommand(
-    "terrastate.untaint",
-    terrastateProvider.untaint.bind(terrastateProvider)
+  context.subscriptions.push(
+    vscode.commands.registerCommand(
+      "terrastate.taint",
+      terrastateProvider.taint.bind(terrastateProvider)
+    )
   );
 
-  vscode.commands.registerCommand(
-    "terrastate.sync",
-    terrastateProvider.sync.bind(terrastateProvider)
+  context.subscriptions.push(
+    vscode.commands.registerCommand(
+      "terrastate.untaint",
+      terrastateProvider.untaint.bind(terrastateProvider)
+    )
   );
 
-  vscode.commands.registerCommand("terrastate.graph", async (arg: string) => {
-    const panel = vscode.window.createWebviewPanel(
-      "Graph",
-      "Terrastate – Graph",
-      vscode.ViewColumn.One,
-      { enableScripts: true }
-    );
-    panel.iconPath = vscode.Uri.joinPath(
-      context.extensionUri,
-      "media/terrastate.png"
-    );
-    panel.webview.html = `
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-    </head>
-      <body>
-        <div id="main"></div>
-        <script src="https://d3js.org/d3.v5.min.js"></script>
-        <script src="https://unpkg.com/@hpcc-js/wasm@0.3.11/dist/index.min.js"></script>
-        <script src="https://unpkg.com/d3-graphviz@3.0.5/build/d3-graphviz.js"></script>
-        <script>
-          const dot = ${JSON.stringify(await graph(arg))}
-          d3.select("#main").graphviz().renderDot(dot)
-        </script>
-      </body>
-    </html>
+  context.subscriptions.push(
+    vscode.commands.registerCommand(
+      "terrastate.sync",
+      terrastateProvider.sync.bind(terrastateProvider)
+    )
+  );
+
+  context.subscriptions.push(
+    vscode.commands.registerCommand("terrastate.graph", async (arg: string) => {
+      const panel = vscode.window.createWebviewPanel(
+        "Graph",
+        "Terrastate – Graph",
+        vscode.ViewColumn.One,
+        { enableScripts: true }
+      );
+      panel.iconPath = vscode.Uri.joinPath(
+        context.extensionUri,
+        "media/terrastate.png"
+      );
+      panel.webview.html = `
+      <!DOCTYPE html>
+      <html lang="en">
+        <head>
+        </head>
+        <body>
+          <div id="main"></div>
+          <script src="https://d3js.org/d3.v5.min.js"></script>
+          <script src="https://unpkg.com/@hpcc-js/wasm@0.3.11/dist/index.min.js"></script>
+          <script src="https://unpkg.com/d3-graphviz@3.0.5/build/d3-graphviz.js"></script>
+          <script>
+            const dot = ${JSON.stringify(await graph(arg))}
+            d3.select("#main").graphviz().renderDot(dot)
+          </script>
+        </body>
+      </html>
     `;
-  });
+    })
+  );
 }
